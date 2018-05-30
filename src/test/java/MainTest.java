0cc0759cd33b5135e1c3848455b5a81fdb196c43
Yaroslav Baymak <yaroslav.baymak@gmail.com>
2018-05-31 00:00:12 +0300

    Update version

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test1(){
+        assert true;
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("true = " + true);
+        assert true;
+    }
+}
